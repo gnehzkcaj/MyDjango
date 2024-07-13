8d0d8c1ebb4e60e49bc9bf66fdbed4d7fad14746
gnehzkcaj <97222543+gnehzkcaj@users.noreply.github.com>
2024-07-13 07:01:42 +0000

    Writing your first Django app, part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,29 @@
 from django.http import HttpResponse
-from django.utils import timezone
+# from django.utils import timezone
+from django.shortcuts import get_object_or_404, render
+from .models import Question
+# from django.template import loader
+
+# def index(request):
+#     current_time = timezone.localtime(timezone.now()).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+#     response_text = f"Hello, world. You're at the polls index. Current time is: {current_time}"
+#     return HttpResponse(response_text)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 
 def index(request):
-    current_time = timezone.localtime(timezone.now()).strftime('%Y-%m-%d %H:%M:%S %Z%z')
-    response_text = f"Hello, world. You're at the polls index. Current time is: {current_time}"
-    return HttpResponse(response_text)
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
