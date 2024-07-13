6f0960f48bb2c50af47b9be9ad2d01b9fecffb6e
gnehzkcaj <97222543+gnehzkcaj@users.noreply.github.com>
2024-07-13 02:31:31 +0000

    Introducing the Django Admin

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+from django.utils import timezone
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    current_time = timezone.localtime(timezone.now()).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+    response_text = f"Hello, world. You're at the polls index. Current time is: {current_time}"
+    return HttpResponse(response_text)
